c751152b5614dcde91c57b8212ad8e39d2945a51
codewithashith <codewithashith@gmail.com>
2023-02-09 12:21:38 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// npm i axios
+
+// Axios
+// returns promise
+// response data located in data object
+// error in response.error
+
+const App = () => {
+  return (
+    <div>
+      <header>Learn React</header>
+    </div>
+  );
+};
+
+export default App;
